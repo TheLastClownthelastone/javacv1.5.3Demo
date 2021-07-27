d901f8ecc4ad148e5dac7f3f37ec191a61a4cf77
pt <2286931835@qq.com>
2021-07-27 15:42:13 +0800

    Initial commit

---
+++ src/main/java/com/pt/javacv/Apng.java
@@ -0,0 +1,14 @@
+package com.pt.javacv;
+
+/**
+ * @author nate-pt
+ * @date 2021/7/27 15:41
+ * @Since 1.8
+ * @Description 将媒体流转成apng
+ */
+public class Apng {
+
+    public static void main(String[] args) {
+        
+    }
+}
